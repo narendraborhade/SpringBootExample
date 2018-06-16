834f36d05efa38d88ccde22184f5f86cd1500a62
Narendra Borhade <narendraborhade@gmail.com>
2018-06-16 20:34:56 +0530

    Spring Boot Example

---
+++ src/main/java/com/springboot/example/SpringBootExample.java
@@ -0,0 +1,18 @@
+/**
+ * @author Narendra Borhade
+ *
+ */
+package com.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  
+public class SpringBootExample {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootExample.class, args);  
+	}
+
+}
