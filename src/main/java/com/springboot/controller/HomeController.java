105b39afae15b2f72aa71a807899b83014244fe3
Narendra Borhade <narendraborhade@gmail.com>
2018-08-03 20:05:00 +0530

    Spring Boot Example

---
+++ src/main/java/com/springboot/controller/HomeController.java
@@ -0,0 +1,17 @@
+/**
+ * @author Narendra Borhade
+ *
+ */
+package com.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(){
+		return "Welcome in Home";
+	}
+}
